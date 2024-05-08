894e5a40372090285502310da1029bd4253a3bcb
¨Jane <smoreira11@hotmail.com>
2024-05-08 22:40:03 +0900

    redy!! add css and japanese translate!

--- script.js
+++ script.js
@@ -2,34 +2,53 @@ const numbers = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("output");
 
-const romanNU = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I']
-  ];
-  
-  convertBtn.addEventListener("click", result);
+function convert(num) { 
+  if(num < 1){ return "";}
+  if(num >= 1000){ return "M" + convert(num - 1000);}
+  if(num >= 900){ return "CM" + convert(num - 900);}
+  if(num >= 500){ return "D" + convert(num - 500);}
+  if(num >= 400){ return "CD" + convert(num - 400);}
+  if(num >= 100){ return "C" + convert(num - 100);}
+  if(num >= 50){ return "L" + convert(num - 50);}
+  if(num >= 40){ return "XL" + convert(num - 40);}
+  if(num >= 10){ return "X" + convert(num - 10);}
+  if(num >= 9){ return "IX" + convert(num - 9);}
+  if(num >= 5){ return "V" + convert(num - 5);}
+  if(num >= 4){ return "IV" + convert(num - 4);}
+  if(num >= 1){ return "I" + convert(num - 1);}  
+}
+const resultElement = document.getElementById('output')
+convertBtn.addEventListener('click', () => {
+    const inputValue = parseInt(numbers.value);
+    const convertedNumber = convert(inputValue);
+    resultElement.textContent = convertedNumber;
+});
+  const checkUserInput = () => {
+  const inputInt = parseInt(numbers.value);
 
-   
-
-  function convertToRoman(num) {
-    if (num === 0) {
-      return '';
-    }
-    for (var i = 0; i < romanNU.length; i++) {
-      if (num >= romanNU[i][0]) {
-        return romanNU[i][1] + convertToRoman(num - romanNU[i][0]);
-      }
-    }
+  if (!numbers.value || isNaN(inputInt)) {
+    result.innerHTML = "<h1>Please enter a valid number</h1>";
+    return;
   }
 
+  if (inputInt === -1) {
+    result.innerHTML = "Please enter a number greater than or equal to 1";
+    return;
+  } if (inputInt === 4000) {
+    result.innerHTML = "Please enter a number less than or equal to 3999";
+    return;
+  } if (inputInt === 3999) {
+    result.innerHTML = "MMMCMXCIX";
+    return;
+  } 
+  
+  
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numbers.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
